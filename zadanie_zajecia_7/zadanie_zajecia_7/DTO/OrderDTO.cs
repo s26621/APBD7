fcd5e5add925d42c16022d0aad82f38a6030e05e
Loczo <truskawka643@gmail.com>
2024-04-22 11:37:37 +0200

    poczatki

---
+++ zadanie_zajecia_7/zadanie_zajecia_7/DTO/OrderDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace zadanie_zajecia_7.DTO;
+
+public record OrderDTO(
+    int IdOrder,
+    int IdProduct,
+    int Amount,
+    DateTime CreatedAt
+    );
---
+++ zadanie_zajecia_7/zadanie_zajecia_7/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace zadanie_zajecia_7.Models;
+
+public class Order
+{
+    [Required]
+    public int IdOrder { get; set; }
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    public int Amount { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    public Order(int idOrder, int idProduct, int amount, DateTime createdAt)
+    {
+        IdOrder = idOrder;
+        IdProduct = idProduct;
+        Amount = amount;
+        CreatedAt = createdAt;
+    }
+
+    public void Deconstruct(out int idOrder, out int idProduct, out int amount, out DateTime createdAt)
+    {
+        idOrder = IdOrder;
+        idProduct = IdProduct;
+        amount = Amount;
+        createdAt = CreatedAt;
+    }
+}
